6fdb10b5a6ff3d2a87feeb9e043375eb0fdc2936
sachp1 <32334065+sachp1@users.noreply.github.com>
2017-11-28 15:07:12 +0530

    Update task1B_main.py

--- task1B_main.py
+++ task1B_main.py
@@ -59,7 +59,7 @@ def main(path):
                 #c2 = c2.dtype("int")
                 cv2.drawContours(sh[i], approx, -1, (255, 0, 255),2)   
                 area = cv2.contourArea(c2)
-                print "\n area of : sh ",i,"  is ", area,"px x px"
+        #        print "\n area of : sh ",i,"  is ", area,"px x px"
                 areashape[i]=area
         #        cv2.imshow("okay",sh[i])
         #        cv2.waitKey(0)
@@ -69,50 +69,66 @@ def main(path):
 
 
         def preprocessing(self):                                            #the Fucntion preprocessing is declared                              
-            lower=np.array([100,100,100],dtype=np.uint16)
-            upper=np.array([255,255,255],dtype=np.uint16)       
-            mask=cv2.inRange(self.img,lower,upper)                                  #Masks the BGR image between the values lower and upper 
-            tres=cv2.threshold(mask,100,255,cv2.THRESH_BINARY)[1]                       #apply threshold
-            invmask=cv2.bitwise_not(tres)                                               #the mask is inverted
-            cont=cv2.findContours(invmask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)                                              #the thresholded image is shown 
-            cont = cont[1] 
+            #lower=np.array([100,100,100],dtype=np.uint16)
+            #upper=np.array([255,255,255],dtype=np.uint16)       
+            #mask=cv2.inRange(self.img,lower,upper)   
+            #nott=cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)                               #Masks the BGR image between the values lower and upper 
+            #tres=cv2.threshold(nott,250,255,cv2.THRESH_TRIANGLE)[1]
+            # tres=cv2.threshold(mask,200,255,cv2.THRESH_BINARY)[1]     
+            blurrd=cv2.medianBlur(self.img,5)
+            edges=cv2.Canny(blurrd,150,200)                    #apply threshold
+            #invmask=cv2.bitwise_not(tres)
+            cv2.imshow("threwe",edges)   
+            #tres2=invmask  
+                                                     #the mask is inverted
+            __,cont,hir=cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)    
+                                                      #the thresholded image is shown 
             #self.areacalc()
-            for c in cont:                                                              #the moments are found
-                M=cv2.moments(c)
-                if(M["m00"]==0):                                        
-                    break                                                   #The fuction breaks if divide by zero condition occurs
-                cval=''                                                     #the value for color is intitally a blank space
-                cx=int((M["m10"]/M["m00"]))
-                cy=int((M["m01"]/M["m00"]))
-                shape=self.detect(c)                              #the shape of each contour is returned by the function detect inside the class ShapeDetector
-                col=hsv[cy][cx][0]                              #the hue value of each contour is given to the function col
+            print"length of countoours is : " ,len(cont)
+            i=0
+            for c in cont:  
+                if (i%2 ==0):                                                          
+                    M=cv2.moments(c)                                             #the moments are found
+                    if(M["m00"]==0):                                        
+                        break                                                   #The fuction breaks if divide by zero condition occurs
+                    cval=''                                                     #the value for color is intitally a blank space
+                    cx=int((M["m10"]/M["m00"]))
+                    cy=int((M["m01"]/M["m00"]))
+                    shape=self.detect(c)                              #the shape of each contour is returned by the function detect inside the class ShapeDetector
+                    if (shape=="rectangle" or shape=="square"):
+                        x,y,w,h=cv2.boundingRect(c)
+                        cx=x+10
+                        cy=y+10
+                    col=hsv[cy][cx][0]                              #the hue value of each contour is given to the function col
 
-                if((col>=0)&(col<5)):
-                    cval="red"
-                elif((col>=5)&(col<30)):
-                    cval="orange"
-                elif((col>=30)&(col<56)):
-                    cval="yellow"
-                elif((col>=56)&(col<128)):                      #The H values of (H,S,V) are checked to find out the color
-                    cval="green"
-                elif((col>=128)&(col<213)):
-                    cval="blue"
-                siz=""
-                c = c.astype("float")
-                c = c.astype("int")
-                cv2.drawContours(img, [c], -1, (255, 0, 255), 2)   
-                area = cv2.contourArea(c)
-                print area 
-                areash=self.areacalc(shape)
-                if((area/areash[0])>=0.95):
-                    siz="large"
-                elif((area/areashape[1])<=1.05):
-                    siz="small"
-                else:
-                    siz="medium"
-                sshape=siz+" "+cval+" "+shape
-                cv2.putText(img, sshape, (cx, cy), cv2.FONT_HERSHEY_SIMPLEX,0.35, (15, 50, 60), 1)  #the required text is written
-                cv2.imshow("masked",img)    
+                    if((col>=0)&(col<5)):
+                        cval="red"
+                    elif((col>=5)&(col<30)):
+                        cval="orange"
+                    elif((col>=30)&(col<56)):
+                        cval="yellow"
+                    elif((col>=56)&(col<128)):                      #The H values of (H,S,V) are checked to find out the color
+                        cval="green"
+                    elif((col>=128)&(col<213)):
+                        cval="blue"
+                    siz=""
+                    c = c.astype("float")
+                    c = c.astype("int")
+                    cv2.drawContours(img, [c], -1, (255, 0, 255), 2)   
+                    area = cv2.contourArea(c)
+                    print area 
+                    areash=self.areacalc(shape)
+                    if((area/areash[0])>=0.95):
+                        siz="large"
+                    elif((area/areashape[1])<=1.05):
+                        siz="small"
+                    else:
+                        siz="medium"
+                    sshape=siz+" "+cval+" "+shape
+                    cv2.putText(img, sshape, (cx, cy), cv2.FONT_HERSHEY_SIMPLEX,0.35, (15, 50, 60), 1)  #the required text is written
+                    cv2.imshow("masked",img)
+                    print "\n"+sshape
+                i+=1 
     im=cv2.imread(path)   
     img=im                            #the final image with the color and shape of each contour is given as output
     hsv=cv2.cvtColor(im,cv2.COLOR_BGR2HSV_FULL)                        #hsv is img in HSV format  
@@ -121,7 +137,7 @@ def main(path):
     cv2.waitKey(0)                                           #waits for an imput fromm the user
     pt=path[2:]
     pt2=pt.replace("test","output")
-    cv2.imwrite(pt2,img,[cv2.IMWRITE_PNG_STRATEGY_DEFAULT,9])
+   # cv2.imwrite(pt2,img,[cv2.IMWRITE_PNG_STRATEGY_DEFAULT,9])
     cv2.destroyAllWindows()
     return pt
 
